f17ee8d234a0741db3f2a64ff0443a50eb3d01ff
xkicken <antonylin123@gmail.com>
2020-07-23 00:08:32 +1200

    added files
    added the files and lib ardunio

---
+++ mazebot/mazebot.ino
@@ -0,0 +1,93 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       mazebot.ino
+    Created:	22/07/2020 11:14:32 PM
+    Author:     DESKTOP-N46U3RV\anton
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+#include <MeWifi.h>
+#include <MeVoice.h>
+#include <MeUSBHost.h>
+#include <MeUltrasonicSensor.h>
+#include <MeTouchSensor.h>
+#include <MeTemperature.h>
+#include <MeStepperOnBoard.h>
+#include <MeStepper.h>
+#include <MeSoundSensor.h>
+#include <MeSmartServo.h>
+#include <MeShutter.h>
+#include <MeSerial.h>
+#include <MeShield.h>
+#include <MeRGBLed.h>
+#include <MePS2.h>
+#include <MePressureSensor.h>
+#include <MePotentiometer.h>
+#include <MePm25Sensor.h>
+#include <MePort.h>
+#include <MePIRMotionSensor.h>
+#include <MeOrion.h>
+#include <MeOneWire.h>
+#include <MeOnBoardTemp.h>
+#include <MeMegaPiProESCMotor.h>
+#include <MeMegaPiPro4DcMotor.h>
+#include <MeMegaPiPro.h>
+#include <MeMegaPiDCMotor.h>
+#include <MeMegaPi.h>
+#include <MeMCore.h>
+#include <MeMbotDCMotor.h>
+#include <MeLineFollower.h>
+#include <MeLimitSwitch.h>
+#include <MeLightSensor.h>
+#include <MeLEDMatrixData.h>
+#include <MeLEDMatrix.h>
+#include <MeJoystick.h>
+#include <MeIR.h>
+#include <MeInfraredReceiver.h>
+#include <MeHumitureSensor.h>
+#include <MeHostParser.h>
+#include <MeGyro.h>
+#include <MeGasSensor.h>
+#include <MeFlameSensor.h>
+#include <MeEncoderOnBoard.h>
+#include <MeEncoderNew.h>
+#include <MeEncoderMotor.h>
+#include <MeEEPROM.h>
+#include <MeDCMotor.h>
+#include <MeConfig.h>
+#include <MeCompass.h>
+#include <MeColorSensor.h>
+#include <MeBuzzer.h>
+#include <MeBluetooth.h>
+#include <MeBaseBoard.h>
+#include <MeAuriga.h>
+#include <Me7SegmentDisplay.h>
+#include <Me4Button.h>
+
+// define ports
+MeDCMotor motor1(M1);
+MeDCMotor motor2(m2);
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+  
+   
+}
