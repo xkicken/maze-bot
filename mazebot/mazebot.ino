23566311e381b9b56dee24e0de2be0a99b1805f6
xkicken <61647663+xkicken@users.noreply.github.com>
2020-09-10 10:28:50 +1200

    added line following code

--- mazebot/mazebot.ino
+++ mazebot/mazebot.ino
@@ -1,93 +1,49 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       mazebot.ino
-    Created:	22/07/2020 11:14:32 PM
-    Author:     DESKTOP-N46U3RV\anton
-*/
-
-// Define User Types below here or use a .h file
-//
-
-
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
-//
-
-// The setup() function runs once each time the micro-controller starts
-#include <MeWifi.h>
-#include <MeVoice.h>
-#include <MeUSBHost.h>
-#include <MeUltrasonicSensor.h>
-#include <MeTouchSensor.h>
-#include <MeTemperature.h>
-#include <MeStepperOnBoard.h>
-#include <MeStepper.h>
-#include <MeSoundSensor.h>
-#include <MeSmartServo.h>
-#include <MeShutter.h>
-#include <MeSerial.h>
-#include <MeShield.h>
-#include <MeRGBLed.h>
-#include <MePS2.h>
-#include <MePressureSensor.h>
-#include <MePotentiometer.h>
-#include <MePm25Sensor.h>
-#include <MePort.h>
-#include <MePIRMotionSensor.h>
-#include <MeOrion.h>
-#include <MeOneWire.h>
-#include <MeOnBoardTemp.h>
-#include <MeMegaPiProESCMotor.h>
-#include <MeMegaPiPro4DcMotor.h>
-#include <MeMegaPiPro.h>
-#include <MeMegaPiDCMotor.h>
-#include <MeMegaPi.h>
 #include <MeMCore.h>
-#include <MeMbotDCMotor.h>
-#include <MeLineFollower.h>
-#include <MeLimitSwitch.h>
-#include <MeLightSensor.h>
-#include <MeLEDMatrixData.h>
-#include <MeLEDMatrix.h>
-#include <MeJoystick.h>
-#include <MeIR.h>
-#include <MeInfraredReceiver.h>
-#include <MeHumitureSensor.h>
-#include <MeHostParser.h>
-#include <MeGyro.h>
-#include <MeGasSensor.h>
-#include <MeFlameSensor.h>
-#include <MeEncoderOnBoard.h>
-#include <MeEncoderNew.h>
-#include <MeEncoderMotor.h>
-#include <MeEEPROM.h>
-#include <MeDCMotor.h>
-#include <MeConfig.h>
-#include <MeCompass.h>
-#include <MeColorSensor.h>
-#include <MeBuzzer.h>
-#include <MeBluetooth.h>
-#include <MeBaseBoard.h>
-#include <MeAuriga.h>
-#include <Me7SegmentDisplay.h>
-#include <Me4Button.h>
 
-// define ports
-MeDCMotor motor1(M1);
-MeDCMotor motor2(m2);
-void setup()
-{
+//Motors
+MeDCMotor m1(M1); //left motor
+MeDCMotor m2(M2); //right motor
 
+//Sensors
+MeUltrasonicSensor ultrasonic(PORT_3);
+MeLineFollower LineSensor(PORT_2);
+
+
+int Sequencestate = 0;
+
+void setup() {
 
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
-{
-  
-   
+void loop() {
+    switch(Sequencestate){
+      case 0:
+        m1.run(0);
+        m2.run(0);
+        Sequencestate = 1;
+        break;
+      case 1:
+        int sensorState = LineSensor.readSensors();
+          switch(sensorState)
+          {
+            case 0:
+              m1.run(-150)
+              m2.run(150);
+              break;
+            case 1:
+              m1.run(0);
+              m2.run(150);
+              break;
+            case 2:
+              m1.run(-150);
+              m2.run(0);
+              break;
+            case 3:
+              m1.run(-100);
+              m2.run(100)
+              break;
+          }
+        break;
+      
+         }
 }
