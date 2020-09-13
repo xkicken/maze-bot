af0bb45e13dfb5d864815265b2460c99f53eaa35
xkicken <61647663+xkicken@users.noreply.github.com>
2020-09-14 10:28:00 +1200

    added leds for switch case debugging

--- mazebot/mazebot.ino
+++ mazebot/mazebot.ino
@@ -8,12 +8,18 @@ MeDCMotor m2(M2); //right motor
 MeUltrasonicSensor ultrasonic(PORT_3);
 MeLineFollower LineSensor(PORT_2);
 
+//other devices
+MeRGBLed led(0, 30);
+
 int Sequencestate = 0;
 
 void setup() {
+led.setpin(13);
+led.reset();
 }
 
 void loop() {
+  led.reset();
     switch(Sequencestate){
       case 0: //idle
         m1.run(0);
@@ -35,13 +41,21 @@ void loop() {
             case 2:
               m1.run(-150);
               m2.run(0);
+              led.setColor(0, 255, 0);
+              led.show();
               break;
             case 3:
+              m1.run(0);
+              m2.run(0);
+              led.setColor(255, 0, 0,);
+              led.show();
               Sequencestate = 2;
               break;
           }
           break;
       case 2: //intersection detection
+        led.setColor(255, 255, 255); //Set both LED to White
+        led.show();
         m1.run(-200);
         m2.run(200);
         if (LineSensor.readSensors() == 0){
