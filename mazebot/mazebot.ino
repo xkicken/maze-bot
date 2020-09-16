693f4c1ccecfe0db5f3f79a4095557592abe6aef
xkicken <61647663+xkicken@users.noreply.github.com>
2020-09-16 12:27:14 +1200

    fixed turnning

--- mazebot/mazebot.ino
+++ mazebot/mazebot.ino
@@ -18,9 +18,11 @@ void setup() {
 led.setpin(13);
 led.setColor(255, 255, 255);
 led.show();
+Serial.begin(9600);
 }
 
 void loop() {
+  Serial.println(Sequencestate);
   sensorState = LineSensor.readSensors();
   led.setColor(0,0,0);
   led.show();
@@ -63,19 +65,38 @@ void loop() {
         led.show();
         m1.run(-200);
         m2.run(200);
-        if (LineSensor.readSensors() == 0){
+        if (LineSensor.readSensors() < 3){ // stop if both sensor sees black
           m1.run(0);
           m2.run(0);
           Sequencestate = 3;
         }
         break;
       case 3:
-        m1.run(0);
+        led.setColor(50, 0, 50); // turn right
+        led.show();
+        m1.run(150);
         m2.run(150);
-        if (LineSensor.readSensors() == 0){
-          m1.run(0);
-          m2.run(0);
+        if (LineSensor.readSensors() == 2){
+        Sequencestate = 4;
         }
         break;
+      case 4:
+        led.setColor(0, 50, 50);
+        led.show();
+        m1.run(150);
+        m2.run(150);
+        if (LineSensor.readSensors() == 0){ //turn untill both sensor is black
+          m1.run(0);
+          m2.run(0);
+          Sequencestate = 5;
+        }
+        break;
+      case 5:
+        if(ultrasonic.distanceCm() > 7){
+          Sequencestate = 1;
+        }
+        else{
+          Sequencestate = 6;
+        }
          }
 }
